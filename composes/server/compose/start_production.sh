b308debabe595bcda22e7b88a6ed8eee41bb4958
Seva <vsdudakov@gmail.com>
2020-05-02 01:32:26 +0300

    Fixed docker compose files (#4)

--- composes/server/compose/start_production.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-python manage.py collectstatic --noinput
-python manage.py migrate --noinput
-python manage.py compilemessages --locale ru
-uwsgi --ini ./compose/uwsgi_production.ini
