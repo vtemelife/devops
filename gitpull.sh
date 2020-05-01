829d92b82fcdfcb195a0a630fb0370da80418a13
vsdudakov <vsdudakov@gmail.com>
2020-05-02 01:31:39 +0300

    Fixed docker compose files

--- gitpull.sh
+++ gitpull.sh
@@ -2,3 +2,4 @@ cd ../client && git checkout develop && git pull origin develop
 cd ../server && git checkout develop && git pull origin develop
 cd ../sockjs && git checkout develop && git pull origin develop
 cd ../mobile && git checkout develop && git pull origin develop
+cd ../devops && git checkout develop && git pull origin develop
