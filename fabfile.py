43ed4867fdb80e59fe23fee4b17fdff2704f371a
vsdudakov <vsdudakov@gmail.com>
2020-05-02 09:19:29 +0300

    Added backups containers

--- fabfile.py
+++ fabfile.py
@@ -13,33 +13,6 @@ LOCAL_DEVOPS_PATH = os.path.join(LOCAL_PROJECT_PATH, 'devops')
 REMOTE_HOST = os.environ['REMOTE_HOST']
 REMOTE_PROJECT_PATH = os.environ['REMOTE_PROJECT_PATH']
 
-
-@task(hosts=(REMOTE_HOST, ))
-def backup(c):
-    date = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M")
-    c.sudo(
-        'zip -r server_media_{date}.zip /var/lib/docker/volumes/vteme_media_data/'.format(  # noqa
-            date=date
-        ),
-        shell=False
-    )
-    c.run(
-        'mv server_media_{date}.zip {path}/'.format(
-            date=date,
-            path=REMOTE_PATH_BACKUPS
-        )
-    )
-    with c.cd(REMOTE_PATH_SITE):
-        c.run('COMPOSE_INTERACTIVE_NO_CLI=1 docker-compose exec postgres pg_dumpall -c -U postgresuser > db_{date}.sql'.format(  # noqa
-            date=date))
-        c.run('zip db_{date}.zip db_{date}.sql'.format(
-            date=date))
-        c.run('rm -f db_{date}.sql'.format(
-            date=date))
-        c.run('mv db_{date}.zip {path}/'.format(
-            date=date, path=REMOTE_PATH_BACKUPS))
-
-
 SERVER_RSYNC_OPTS = dict(
     rsync_opts='--filter=":- .gitignore"',
     exclude=[
@@ -77,13 +50,51 @@ def clean(connection, instance=None):
 
 
 @task(hosts=(REMOTE_HOST, ))
-def deploy(connection, instance=None):
+def backup(connection, instance=None):
     instance = instance or 'staging'
     remote_path = os.path.join(REMOTE_PROJECT_PATH, instance)
     with connection.cd(remote_path):
         _run(connection, 'docker-compose build')
-        _run(connection, 'docker-compose stop')
-        _run(connection, 'docker-compose up -d')
+        _run(connection, 'docker-compose run backup')
+
+
+@task(hosts=(REMOTE_HOST, ))
+def deployclient(connection, instance=None):
+    instance = instance or 'staging'
+    os.system("cd {} && make build".format(LOCAL_CLIENT_PATH))
+    remote_path = os.path.join(REMOTE_PROJECT_PATH, instance, 'client')
+    _run(connection, 'mkdir -p {}'.format(remote_path))
+    rsync(
+        connection,
+        os.path.join(LOCAL_CLIENT_PATH, 'build'),
+        remote_path,
+    )
+
+
+@task(hosts=(REMOTE_HOST, ))
+def deployserver(connection, instance=None):
+    instance = instance or 'staging'
+    remote_path = os.path.join(REMOTE_PROJECT_PATH, instance)
+    _run(connection, 'mkdir -p {}'.format(remote_path))
+    rsync(
+        connection,
+        LOCAL_SERVER_PATH,
+        remote_path,
+        **SERVER_RSYNC_OPTS,
+    )
+
+
+@task(hosts=(REMOTE_HOST, ))
+def deploysockjs(connection, instance=None):
+    instance = instance or 'staging'
+    remote_path = os.path.join(REMOTE_PROJECT_PATH, instance)
+    _run(connection, 'mkdir -p {}'.format(remote_path))
+    rsync(
+        connection,
+        LOCAL_SOCKJS_PATH,
+        remote_path,
+        **SERVER_RSYNC_OPTS,
+    )
 
 
 @task(hosts=(REMOTE_HOST, ))
@@ -130,6 +141,12 @@ def deploydevops(connection, instance=None):
         os.path.join(LOCAL_DEVOPS_PATH, 'composes', 'client', 'compose'),
         os.path.join(remote_path, 'client'),
     )
+    _run(connection, 'mkdir -p {}'.format(os.path.join(remote_path, 'backups')))
+    rsync(
+        connection,
+        os.path.join(LOCAL_DEVOPS_PATH, 'composes', 'backups', 'compose'),
+        os.path.join(remote_path, 'backups'),
+    )
     rsync(
         connection,
         os.path.join(LOCAL_DEVOPS_PATH, 'docker-compose-{}.yml'.format(instance)),
@@ -138,39 +155,15 @@ def deploydevops(connection, instance=None):
 
 
 @task(hosts=(REMOTE_HOST, ))
-def deployclient(connection, instance=None):
-    instance = instance or 'staging'
-    os.system("cd {} && make build".format(LOCAL_CLIENT_PATH))
-    remote_path = os.path.join(REMOTE_PROJECT_PATH, instance, 'client')
-    _run(connection, 'mkdir -p {}'.format(remote_path))
-    rsync(
-        connection,
-        os.path.join(LOCAL_CLIENT_PATH, 'build'),
-        remote_path,
-    )
-
-
-@task(hosts=(REMOTE_HOST, ))
-def deployserver(connection, instance=None):
+def deploy(connection, instance=None):
+    deployclient(connection, instance=instance)
+    deployserver(connection, instance=instance)
+    deploysockjs(connection, instance=instance)
+    deploydevops(connection, instance=instance)
     instance = instance or 'staging'
     remote_path = os.path.join(REMOTE_PROJECT_PATH, instance)
-    _run(connection, 'mkdir -p {}'.format(remote_path))
-    rsync(
-        connection,
-        LOCAL_SERVER_PATH,
-        remote_path,
-        **SERVER_RSYNC_OPTS,
-    )
+    with connection.cd(remote_path):
+        _run(connection, 'docker-compose build')
+        # _run(connection, 'docker-compose stop')
+        _run(connection, 'docker-compose up -d react')
 
-
-@task(hosts=(REMOTE_HOST, ))
-def deploysockjs(connection, instance=None):
-    instance = instance or 'staging'
-    remote_path = os.path.join(REMOTE_PROJECT_PATH, instance)
-    _run(connection, 'mkdir -p {}'.format(remote_path))
-    rsync(
-        connection,
-        LOCAL_SOCKJS_PATH,
-        remote_path,
-        **SERVER_RSYNC_OPTS,
-    )
